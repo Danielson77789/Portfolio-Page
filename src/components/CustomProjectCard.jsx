afe3f0a8f753837d2e7977b5a13a9a340b055d05
Danielson77789 <dpeterson8@ewu.edu>
2023-12-30 23:12:42 -0800

    Added some more animations

--- src/components/CustomProjectCard.jsx
+++ src/components/CustomProjectCard.jsx
@@ -3,13 +3,14 @@ import ProjectFlipCard from './ProjectFlipCard';
 
 
 
-const CustomProjectCard = ({projectType, title, description, side, img}) => {
+const CustomProjectCard = ({projectType, title, description, side, img, position}) => {
 
   return (
     <div className='body'>
       {side==="left" && <ProjectFlipCard projectType={projectType} title={title} description={description}/>}
       <div className='img-container'>
-        <img src={img} alt='...' className='img'/>
+        {side==="left" && <img src={img} alt='...' className={`imgL ${position}`}/>}
+        {side==="right" && <img src={img} alt='...' className='imgR'/>}
       </div>
       {side==="right" && <ProjectFlipCard projectType={projectType} title={title} description={description}/>}
     </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import '../components/css/Projects.css'
 import bloomFlip from '../components/assets/bloom-flip.gif'
 import towerDefence from '../components/assets/tower-def.gif'
+import random from '../components/assets/random.gif'
 import { PiVault } from "react-icons/pi";
 
 
@@ -14,6 +15,7 @@ const Projects = () => {
         <h2 className='header'>
           Project<br/>Vault<PiVault/>
         </h2>
+        <div className='header-line'/>
       </div>
       <div className='projects-container'>
 
@@ -23,12 +25,12 @@ const Projects = () => {
           </div>
 
           <div className='card-four'>
-              <CustomProjectCard img={bloomFlip} side="right" title="Simple Neural Network" projectType="C Cuda Development" description="This project was a final for a GPU Computing class where we learned to use cuda framework in C to utilize the GPU computation 
+              <CustomProjectCard img={random} side="right" title="Simple Neural Network" projectType="C Cuda Development" description="This project was a final for a GPU Computing class where we learned to use cuda framework in C to utilize the GPU computation 
               power on various projects throughout the quarter. For are final project we chose to attempt a simple neural network. This network was implemented both with and without cuda."/>
           </div>
 
           <div className='card-four'>
-              <CustomProjectCard img={towerDefence} side="left" title="Tower Defense" projectType="Unity Game Development" description="In this unity project that me and my team had put together for are software architecture class I learned processes that involve
+              <CustomProjectCard position='pos2' img={towerDefence} side="left" title="Tower Defense" projectType="Unity Game Development" description="In this unity project that me and my team had put together for are software architecture class I learned processes that involve
               application development. Some of the key concepts of this projects were the development operations and design practices used to achieve the final product."/>
           </div>
 
